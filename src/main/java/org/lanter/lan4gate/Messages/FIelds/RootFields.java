ca4908d793e8479d3dd5df8944fbb6c459de4cd2
Lysenkov <lysenkov@lanit.ru>
2020-04-13 04:06:13 +0300

    Удалены комметарии в кодировке 1251

--- src/main/java/org/lanter/lan4gate/Messages/FIelds/RootFields.java
+++ src/main/java/org/lanter/lan4gate/Messages/FIelds/RootFields.java
@@ -1,12 +1,6 @@
 package org.lanter.lan4gate.Messages.Fields;
 
-/**
- * Имена корневых полей JSON сообщения
- * @author Lysenkov Vladimir
- */
 public class RootFields {
-    /** Имя поля класса сообщения */
     public static final String CLASS = "__class";
-    /** Имя поля объекта сообщения */
     public static final String OBJECT = "__object";
 }
