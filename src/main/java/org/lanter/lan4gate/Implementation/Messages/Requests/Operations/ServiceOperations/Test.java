4177fb41c6ffaf7c4fd26f4f29e3df63c84f4f19
Лысенков Владимир <Lysenkov@lanit>
2020-10-15 16:28:18 +0300

    Добавлено опциональное поле EcrMerchantNumber для операции Test

--- src/main/java/org/lanter/lan4gate/Implementation/Messages/Requests/Operations/ServiceOperations/Test.java
+++ src/main/java/org/lanter/lan4gate/Implementation/Messages/Requests/Operations/ServiceOperations/Test.java
@@ -1,10 +1,12 @@
 package org.lanter.lan4gate.Implementation.Messages.Requests.Operations.ServiceOperations;
 
+import org.lanter.lan4gate.Messages.Fields.RequestFieldsList;
 import org.lanter.lan4gate.Messages.OperationsList;
 import org.lanter.lan4gate.Implementation.Messages.Requests.Request;
 
 public class Test extends Request {
     public Test() {
         setOperationCode(OperationsList.Test);
+        addOptionalFields(RequestFieldsList.EcrMerchantNumber);
     }
 }
