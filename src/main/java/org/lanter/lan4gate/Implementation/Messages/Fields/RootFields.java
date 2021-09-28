4ff8540acb2299d908e14e173ee04895163a078e
Vladimir Lysenkov <vladimir-lysenkov93@yandex.ru>
2021-09-28 06:52:16 +0300

    fix Удалены лишние enum

--- src/main/java/org/lanter/lan4gate/Implementation/Messages/Fields/RootFields.java
+++
@@ -1,6 +0,0 @@
-package org.lanter.lan4gate.Implementation.Messages.Fields;
-
-public class RootFields {
-    public static final String CLASS = "__class";
-    public static final String OBJECT = "__object";
-}
